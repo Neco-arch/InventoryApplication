f6c0504afd858e5e2c2807f2672fddd457e33c6c
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2026-04-05 12:31:02 +0700

    feat : make database readable

--- db/queries.js
+++ db/queries.js
@@ -1,13 +1,15 @@
 const pool = require('./pool.js')
 
-async function FliterData() {
-
+async function InsertData(itemname, description, category, price, quantity, status) {
+    await pool.query("INSERT INTO streetwear_products (itemname , ) VALUES ($1 , $2 )", itemname    )
 }
 
 async function GetData() {
-
+    const { rows } = await pool.query("SELECT * FROM streetwear_products")
+    return rows
 }
 
 module.exports = {
-    TestDB
+    InsertData,
+    GetData
 }
