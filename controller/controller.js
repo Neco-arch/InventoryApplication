bb8a14951d0ca4baaab587ef556880c768db97ab
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2026-04-09 16:41:06 +0700

    feat : controller contact db

--- controller/controller.js
+++ controller/controller.js
@@ -1,2 +1,44 @@
 const queries = require('../db/queries.js')
 
+//CreateProduct
+async function CreateProduct(itemname , description , category , price , quantity , status ) {
+    const ProductData = await queries.GetData()
+
+
+    //Checking Data
+    const CheckDupe = ProductData.some((value , index) => {
+        value.itemname === itemname ? true : false
+    })
+
+    if (CheckDupe === true ) {
+        return "Product Dupe"
+    }
+    // Insert Product
+    const Result = await queries.InsertData(itemname,description,category,price,quantity,status)
+    return Result
+}
+
+//EditData 
+async function EditProduct(ItemID,NewItemname , Newdescription , newcategory , newprice , newquantity, newstatus) {
+    if (newcategory = null) {
+        newcategory = "none"
+    }
+    const result = await queries.UpdateData(ItemID,NewItemname,Newdescription,newcategory,newprice,newquantity,newstatus)
+    return result
+}
+
+
+//Delete Product
+async function DeleteProduct(ItemID,Itemname) {
+    const result = await queries.DeleteData(ItemID,Itemname)
+    return result
+}
+
+ay
+
+
+module.exports = {
+    CreateProduct,
+    EditProduct,
+    DeleteProduct,
+}
--- db/queries.js
+++ db/queries.js
@@ -12,10 +12,14 @@ async function InsertData(
   const querymessage =
     "INSERT INTO products(itemname, description,category,price,quantity,status) VALUES($1, $2 , $3 , $4 , $5 , $6)";
   const value = [itemname, description, category, price, quantity, status];
-  await pool.query(querymessage, value);
+  const querymessage_2 = "INSERT INTO allcategory(category) VALUES($1)";
+  const result1 = await pool.query(querymessage, value);
+  const result2 = await pool.query(querymessage_2, category);
+
+  return [result1, result2];
 }
 
-async function GetData() {
+async function GetAllData() {
   try {
     const { rows } = await pool.query("SELECT * FROM products");
     return rows;
@@ -25,11 +29,11 @@ async function GetData() {
   }
 }
 
-async function GetDataSpecific(id) {
+async function SortData(category, price, status) {
   try {
     const { rows } = await pool.query(
-      "SELECT * FROM products WHERE id = ($1)",
-      id,
+      "SELECT * FROM products WHERE category = ($1) OR price = ($2) OR status = ($3) ",
+      [category,price,status]
     );
     return rows;
   } catch (err) {
@@ -39,7 +43,7 @@ async function GetDataSpecific(id) {
 }
 
 async function UpdateData(
-    Dataid,
+  Dataid,
   itemname,
   description,
   category,
@@ -47,22 +51,19 @@ async function UpdateData(
   quantity,
   status,
 ) {
-    const querymessage = 'UPDATE products SET itemname = ($1) , description = ($2) , category = ($3) , price = ($4) , quantity = ($5) , status = ($6) WHERE id = ($7)'
-    const value = [itemname,description,category,price,quantity,status,Dataid]
-    try {
-        const result = pool.query(querymessage,value)
-        return result
-    } catch (err) {
-    console.log("Error Happened");
-    console.log(err);
-    }
-}
-
-async function DeleteData(id) {
+  const querymessage =
+    "UPDATE products SET itemname = ($1) , description = ($2) , category = ($3) , price = ($4) , quantity = ($5) , status = ($6) WHERE id = ($7) ";
+  const value = [
+    itemname,
+    description,
+    category,
+    price,
+    quantity,
+    status,
+    Dataid,
+  ];
   try {
-    const result = await pool.query("DELETE FROM products WHERE id = ($1)", [
-      id,
-    ]);
+    const result = await pool.query(querymessage, value);
     return result;
   } catch (err) {
     console.log("Error Happened");
@@ -70,12 +71,23 @@ async function DeleteData(id) {
   }
 }
 
-
+async function DeleteData(id, itemname) {
+  try {
+    const result = await pool.query(
+      "DELETE FROM products WHERE id = ($1) OR itemname = ($2)",
+      [id, itemname],
+    );
+    return result;
+  } catch (err) {
+    console.log("Error Happened");
+    console.log(err);
+  }
+}
 
 module.exports = {
   InsertData,
-  GetData,
-  GetDataSpecific,
+  GetAllData,
+  SortData,
   DeleteData,
-  UpdateData
+  UpdateData,
 };
